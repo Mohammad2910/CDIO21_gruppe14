a39dda0a70b72aab57dc1361eb90a2eb7783514c
Nafiu <s184174@student.dtu.dk>
2021-02-24 14:27:39 +0100

    Test af camerapreview

--- app/src/main/java/com/example/cdio21_gruppe14/MainActivity.java
+++ app/src/main/java/com/example/cdio21_gruppe14/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.cdio21_gruppe14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
+    private TextView textView1;
+    private TextView textView2;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.navn);
+        textView2 = findViewById(R.id.subNavn);
+        imageView = findViewById(R.id.klondike);
+
+        button = findViewById(R.id.begyndKnap);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == button) {
+            Intent kabale = new Intent(this,KabaleActivity.class);
+            startActivity(kabale);
+        }
     }
 }
